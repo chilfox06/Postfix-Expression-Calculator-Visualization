44961fb88c56326bc0808c0f1422da7dfb457fac
gc-octopis <gcoctopis@gmail.com>
2025-04-29 17:43:27 +0800

    vite enviornment setup

---
+++ postecv/src/types.ts
@@ -0,0 +1,33 @@
+/*
+  import them with syntax:
+
+  import {[type1], [type2], ..., [typen]} from './types.ts'
+
+*/
+
+export type StackArray = number[]; 
+/*
+  like C typedef
+  Define StackArray as an Array contains only numbers.
+  (note: In JS/TS, both int and float count as type 'number')
+*/
+
+export type Op = '+' | '-' | '*' | '/';
+/*
+  Restricting Op to only be those 4 char. (string actually)
+*/
+
+export interface ViewData
+{
+    stack:    StackArray;
+    left:     number;
+    right:    number;
+    operator: Op;
+    ans:      number;
+}
+/*
+  like C struct, but the syntax is
+  [name]: [type];
+*/
+
+export type Expression = (number | Op)[];
